93769042e0d40aba115f431a9e6c8e28d2871b72
Trevor Brindle <trevor@weavegrid.com>
2022-04-13 14:44:21 -0400

    feat: add markup and style stubs for SelectPrograms

--- pages/select-programs.tsx
+++ pages/select-programs.tsx
@@ -1,8 +1,42 @@
-import type { NextPage } from 'next'
-import styles from '../styles/SelectPrograms.module.css'
+import type { NextPage } from "next";
+import styles from "../styles/SelectPrograms.module.css";
 
-const SelectPrograms: NextPage = () => {
-  return <div className={styles.container}>Hello Weavegrid!</div>;
-}
+const SelectPrograms: NextPage = (props) => {
+  return (
+    <div className={styles.sp__container}>
+      <h1 className={styles.sp__header}>Select Programs</h1>
+      <form role="form">
+        <div className={styles.sp__card}>
+          <label htmlFor="${`program-${program.id}`}">
+            <h2 className={styles.sp__card_header}>program.name</h2>
+            <p className={styles.sp__card_aside}>program.exclusionText</p>
+            <input 
+              checked={true} // program.preselected
+              name="program"
+              type="checkbox"
+              onChange={() => {}}
+              value="program id"
+            />
+            <span className={styles.sp__checkbox}></span>
+            <p className={styles.sp__card_description}>program.description</p>
+            <ul>
+              <li><a href="program.links.link">program.links.text</a></li>
+            </ul>
+            <h3 className={styles.sp__card_perks_header}>Perks</h3>
+            <ul>
+              <li>
+                <a className={styles.sp__card_perks_content} href="program.perks.link">program.perks.text</a>
+              </li>
+            </ul>
+            <div className={styles.sp__card_footer}>
+              <p className={styles.sp__card_footer_text}>program.footer</p>
+            </div>
+              <p className={styles.sp__card_terms_text}>program.termsText</p>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default SelectPrograms
+export default SelectPrograms;
